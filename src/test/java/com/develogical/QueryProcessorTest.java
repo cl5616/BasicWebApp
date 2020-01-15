dfc9085c437f81b5cf7dde59fb5dd7968ee0343a
cl5616 <cl5616@ic.ac.uk>
2020-01-15 19:07:28 +0000

    add apple test

--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -24,4 +24,9 @@ public class QueryProcessorTest {
     public void isNotCaseSensitive() throws Exception {
         assertThat(queryProcessor.process("shakespeare"), containsString("playwright"));
     }
+
+    @Test
+    public void appleTest() throws Exception {
+        assertThat(queryProcessor.process("apple"), containsString("apple"));
+    }
 }
